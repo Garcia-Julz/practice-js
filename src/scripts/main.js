f410997989b5fc82a411aa524fa2a411b9bf4d96
Julian Garcia <garcia.j.julian@gmail.com>
2019-10-10 11:44:59 -0500

    beatles work

---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+const mmt = beatles.albums[3]
+const paul = beatles.members[1].name
+
+
+console.log(`${paul} was in the Beatles ${beatles.history.formed} to ${beatles.history.disbanded}. He was born in ${beatles.members[1].birth}. He contributed heavily to the ${mmt} Album.`)
